ee844cbe6bda6f1137e5164a247e7d4257dc45f4
Franco Amon Maldonado <franco.m_887@outlook.com>
2023-07-10 00:45:55 -0300

    10/07/2023
    -Llamado a la api

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -1,13 +1,25 @@
 import { useState, useEffect, createContext } from "react";
+import axios from "axios";
 
 const NoticiasContext = createContext()
 
 const NoticiasProvider = ({ children }) => {
     const [categoria, setCategoria] = useState('general')
+    const [noticias, setNoticias] = useState([])
 
     const handleChangeCategoria = (e)=>{
         setCategoria(e.target.value)
     }
+
+    useEffect(()=>{
+
+        const api = async ()=>{
+            const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&pageSize=100&apiKey=${import.meta.env.VITE_API_KEY}`
+            const {data} = await axios(url)
+            setNoticias(data.articles)
+        }
+        api()
+    },[categoria])
 
     return (
         <NoticiasContext.Provider
